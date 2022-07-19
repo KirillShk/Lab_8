9b3ccbc7423249a3d245b954a01667e24e7c91cb
Kirill Shlyakov <Kirill Shlyakov@DESKTOP-4PP6PL7>
2022-07-20 00:21:38 +0300

    Добавьте файлы проекта.

---
+++ Task_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\BIM\csharp\Lab_8\Files\List_words.txt";
+            string[] filetext = File.ReadAllText(path);
+            int k=0;
+            int z = filetext.Length;
+           /* foreach (char ch in filetext)
+            {
+                k++;
+            }    */    
+           Console.WriteLine(z);
+            Console.WriteLine("Количество символов = {0}", k);
+            Console.ReadLine(); 
+        }
+    }
+}
